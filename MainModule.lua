507ed105aef1c8f34e5779faaa592a2cd962050a
Hat <mashroorhasan2@gmail.com>
2019-04-20 21:37:49 -0400

    Create MainModule.lua

---
+++ MainModule.lua
@@ -0,0 +1,17 @@
+-- This is the parent. All the folders in this repository is a child of this script.
+
+function Load()
+	for _,v in pairs(script:GetChildren()) do
+		if game:FindFirstChild(v.Name) then
+			for _,x in pairs(v:GetChildren()) do
+				x.Parent = game[v.Name]
+			end
+		elseif game.StarterPlayer:FindFirstChild(v.Name) then
+			for _,x in pairs(v:GetChildren()) do
+				x.Parent = game.StarterPlayer[v.Name]
+			end
+		end
+	end
+end
+
+return Load
